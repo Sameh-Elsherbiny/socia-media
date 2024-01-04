5b118ed4b2ab37f4583aa4b296cbe3209cfc2cb9
sameh-Elsherbiny <samehmelsherbiny#gmail.com>
2024-01-04 21:20:45 +0200

    social-media

---
+++ account/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path, include
+from django.contrib.auth import views as auth_views
+from . import views
+
+urlpatterns = [
+    path('', include('django.contrib.auth.urls')),
+    path('', views.dashboard, name='dashboard'),
+    path('register/', views.register, name='register'),
+    path('edit/', views.edit, name='edit'),
+    path('users/', views.user_list, name='user_list'),
+    path('users/follow/', views.user_follow, name='user_follow'),
+    path('users/<username>/', views.user_detail, name='user_detail'),
+]
